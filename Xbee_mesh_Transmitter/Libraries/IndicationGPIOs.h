d0ab8ab16e54bf1c639501932051504236da930d
niklavs <niks.barkovskis@gmail.com>
2016-07-04 12:30:50 +0300

    mesh network project added

---
+++ Xbee_mesh_Transmitter/Libraries/IndicationGPIOs.h
@@ -0,0 +1,44 @@
+#ifndef GPIO_LIBRARY
+#define GPIO_LIBRARY
+
+//These are the Includes
+#include <stm32f0xx_gpio.h>
+#include <stm32f0xx_misc.h>
+#include <stm32f0xx_rcc.h>
+
+//These are the Define statements
+
+//These are the prototypes for the routines
+void initializeGreenLed1(void);
+void initializeGreenLed2(void);
+void initializeGreenLed3(void);
+void initializeRedLed1(void);
+void initializeRedLed2(void);
+void initializeRedLed3(void);
+void initializeRedLed4(void);
+void initializeRedLed5(void);
+//void delayGPIOs(int count);
+
+void blinkGreenLed1(void);
+void blinkGreenLed2(void);
+void blinkGreenLed3(void);
+void blinkRedLed1(void);
+void blinkRedLed2(void);
+void blinkRedLed3(void);
+void blinkRedLed4(void);
+void blinkRedLed5(void);
+
+void turnOnGreenLed1(void);
+void turnOnGreenLed2(void);
+void turnOnGreenLed3(void);
+void turnOnRedLed1(void);
+void turnOnRedLed2(void);
+void turnOnRedLed3(void);
+void turnOnRedLed4(void);
+void turnOnRedLed5(void);
+
+void blinkIndicationLedTwice(void);
+void blinkIndicationLedThrice(void);
+
+#endif
+
